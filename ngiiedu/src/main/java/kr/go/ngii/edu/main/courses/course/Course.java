4fab0cc3bda74de3fa9fbdc756807120eb61ce26
Hanjin Lee <jinifor@gmail.com>
2017-09-13 16:40:50 +0900

    Mapper sample

---
+++ ngiiedu/src/main/java/kr/go/ngii/edu/main/courses/course/Course.java
@@ -0,0 +1,11 @@
+package kr.go.ngii.edu.main.courses.course;
+
+import java.io.Serializable;
+
+public class Course implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+
+
+}
--- ngiiedu/src/main/java/kr/go/ngii/edu/main/courses/course/mapper/CourseMapper.java
+++ ngiiedu/src/main/java/kr/go/ngii/edu/main/courses/course/mapper/CourseMapper.java
@@ -1,5 +1,22 @@
 package kr.go.ngii.edu.main.courses.course.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.go.ngii.edu.main.courses.course.Course;
+
+@Mapper
 public interface CourseMapper {
+
+	public Course get(Course course);
+
+	public List<Course> list(Course course);
+
+	public int create(Course course);
+
+	public void modify(Course course);
+
+	public void delete(int idx);
 
 }
