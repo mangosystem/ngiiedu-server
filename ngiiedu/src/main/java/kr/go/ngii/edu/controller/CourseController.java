4f41af00e9c44f8380f1e3cbde4542646fb8670d
favorson <favorson@gmail.com>
2017-11-09 11:11:25 +0900

    conflict merge

--- ngiiedu/src/main/java/kr/go/ngii/edu/controller/CourseController.java
+++ ngiiedu/src/main/java/kr/go/ngii/edu/controller/CourseController.java
@@ -7,7 +7,6 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
