4a7ea04956ab3059bdc7ee5c5e54aab7f34bda75
favorson <favorson@gmail.com>
2017-11-09 10:28:59 +0900

    REST API ENCODING 추가

--- ngiiedu/src/main/java/kr/go/ngii/edu/main/common/RestAPIClient.java
+++ ngiiedu/src/main/java/kr/go/ngii/edu/main/common/RestAPIClient.java
@@ -4,6 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URLDecoder;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -18,10 +19,16 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.ResourceHttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.converter.support.AllEncompassingFormHttpMessageConverter;
+import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
+import org.springframework.http.converter.xml.SourceHttpMessageConverter;
+import org.springframework.util.ClassUtils;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
@@ -41,10 +48,50 @@ public class RestAPIClient {
 	private static final String REST_PROJECT_ID = LocalResourceBundle.PINOGIO_API_PROJECT_ID;
 	private RestTemplate restTemplate;
 	public RestAPIClient() {
-//		restTemplate = getRestTempalte();
 		restTemplate = new RestTemplate();
-		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-		restTemplate.getMessageConverters().add(converter);
+//		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+//		restTemplate.getMessageConverters().add(converter);
+
+//		String cahrSet = "UTF-8";
+//		List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
+//		FormHttpMessageConverter formHttpMessageConverter = new FormHttpMessageConverter();
+//		formHttpMessageConverter.setCharset(Charset.forName(cahrSet));
+//		List<HttpMessageConverter<?>> partConverters = new ArrayList<HttpMessageConverter<?>>();
+//		partConverters.add(new StringHttpMessageConverter(Charset.forName(cahrSet)));
+//		partConverters.add(new ResourceHttpMessageConverter());
+//		partConverters.add(new ByteArrayHttpMessageConverter());
+//		formHttpMessageConverter.setPartConverters(partConverters);
+//		messageConverters.add(formHttpMessageConverter);
+//		messageConverters.add(new StringHttpMessageConverter(Charset.forName(cahrSet)));
+//		restTemplate = new RestTemplate(messageConverters);
+//		
+//		for (HttpMessageConverter<?> hmc : restTemplate.getMessageConverters()) {
+//		    if (hmc instanceof AllEncompassingFormHttpMessageConverter) {
+//
+//		        /** AllEncompassingFormHttpMessageConverter 생성자 내용 일부 가져와서 수정 **/
+//		        List<HttpMessageConverter<?>> partConverterList = new ArrayList<HttpMessageConverter<?>>();
+//		        partConverterList.add(new ByteArrayHttpMessageConverter());
+//		        StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter(Charset.forName(cahrSet));
+//		        stringHttpMessageConverter.setWriteAcceptCharset(false);
+//		        partConverterList.add(stringHttpMessageConverter);
+//		        partConverterList.add(new ResourceHttpMessageConverter());
+//		        partConverterList.add(new SourceHttpMessageConverter());
+//		        if (ClassUtils.isPresent("javax.xml.bind.Binder", AllEncompassingFormHttpMessageConverter.class.getClassLoader())) {
+//		            partConverterList.add(new Jaxb2RootElementHttpMessageConverter());
+//		        }
+//		        if (ClassUtils.isPresent("com.fasterxml.jackson.databind.ObjectMapper", AllEncompassingFormHttpMessageConverter.class.getClassLoader())
+//		                && ClassUtils.isPresent("com.fasterxml.jackson.core.JsonGenerator", AllEncompassingFormHttpMessageConverter.class.getClassLoader())) {
+//		            partConverterList.add(new MappingJackson2HttpMessageConverter());
+//		        }
+//		        else if (ClassUtils.isPresent("org.codehaus.jackson.map.ObjectMapper", AllEncompassingFormHttpMessageConverter.class.getClassLoader())
+//		                && ClassUtils.isPresent("org.codehaus.jackson.JsonGenerator", AllEncompassingFormHttpMessageConverter.class.getClassLoader())) {
+//		            partConverterList.add(new MappingJackson2HttpMessageConverter());
+//		        }
+//
+//		        ((AllEncompassingFormHttpMessageConverter) hmc).setPartConverters(partConverterList);
+//		    }
+//		}
+		
 	}
 	
 	private RestTemplate getRestTempalte() {
@@ -110,20 +157,25 @@ public class RestAPIClient {
 
 	
 	public Map<String, Object> getResponseBody(EnumRestAPIType EnumRestAPIType, String pathParam, Map<String, String> param) {
-		HttpHeaders headers = new HttpHeaders();
-	    headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
-		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(REST_BASE_URI + pathParam);
-	    for( Map.Entry<String, String> elem : param.entrySet()) {
-	    	builder.queryParam(elem.getKey(), elem.getValue());
-	    }
-	    
-	    URI uriParam = builder.build().toUri();
-	    ParameterizedTypeReference<Map<String, Object>> typeRef = 
-				new ParameterizedTypeReference<Map<String, Object>>() {};
-		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-		ResponseEntity<Map<String, Object>> result = restTemplate.exchange(uriParam, EnumRestAPIType.method(), entity, 
-				typeRef);
-		Map<String, Object> body = result.getBody();
-		return body;
+		try {
+			HttpHeaders headers = new HttpHeaders();
+		    headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+		    UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(REST_BASE_URI + pathParam);
+		    for( Map.Entry<String, String> elem : param.entrySet()) {
+		    	builder.queryParam(elem.getKey(), elem.getValue());
+		    }
+		    URI uriParam = builder.build().encode("UTF-8").toUri();
+		    ParameterizedTypeReference<Map<String, Object>> typeRef = 
+					new ParameterizedTypeReference<Map<String, Object>>() {};
+			HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+			ResponseEntity<Map<String, Object>> result = restTemplate.exchange(uriParam, EnumRestAPIType.method(), entity, 
+					typeRef);
+			Map<String, Object> body = result.getBody();
+		
+			return body;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
